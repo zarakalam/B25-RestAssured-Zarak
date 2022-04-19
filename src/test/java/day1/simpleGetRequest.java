5bfa1a76708be9f13d240f2cd1d01d7f08233fe0
Zarak A <alamz523@gmail.com>
2022-04-19 09:30:55 -0500

    day1.API

---
+++ src/test/java/day1/simpleGetRequest.java
@@ -0,0 +1,29 @@
+package day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+
+public class simpleGetRequest<url> {
+
+    String url = "http://44.201.121.105:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.get(url);
+
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        response.prettyPrint();
+
+    }
+
+
+
+
+
+
+}
